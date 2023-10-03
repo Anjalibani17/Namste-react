dad130b1c0d3b0d15305cf83cea99e6660a86dbf
Anjalibani17 <anjalibani2002@gmail.com>
2023-10-03 19:20:24 +0530

    fetch

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,23 +1,46 @@
 import Restaurantcard from "./Restaurantcard";
-import resList from "../utils/mockData";
-import { useState } from "react";
-import resList from "../utils/mockData";
-const Body = () => {
-    const [listRes,setListRes]=useState(resList);
-    //here we are passing reslist mock data into use state and then update it
+// import resList from "../utils/mockData";
+import { useEffect, useState } from "react";
 
+const Body = () => {
+    const [listRes, setListRes] = useState([]);
+    //here we are passing reslist mock data into use state and then update it
+    useEffect(() => {
+        fetchData();
+    }, []);
+        console.log("body");
+    const fetchData = async () => {
+        const data = await fetch(
+            "https://www.swiggy.com/mapi/homepage/getCards?lat=22.3189294&lng=73.1669011"
+        );
+        // console.log(data)
+        const jsonD = await data.json();
+        console.log(jsonD);
+        console.log(
+            jsonD.data.success.cards[4].gridWidget.gridElements.infoWithStyle
+                .restaurants
+        );
+        //optional chaining 
+        const finalData =
+            jsonD?.data?.success?.cards[4]?.gridWidget?.gridElements?.infoWithStyle?.restaurants;
+
+        setListRes(finalData);
+    };
+    if(listRes.length==0)
+    {
+        return <h2>loading...</h2>;
+    }
     return (
         <div className="body">
             <div className="filter">
                 <button
                     className="filterbtn"
                     onClick={() => {
-                        const filresList = resList.filter(
+                        const filresList = listRes.filter(
                             (res) => res.info.avgRating > 4.2
                         );
                         console.log(filresList);
                         setListRes(filresList);
-   
                     }}
                 >
                     Top rated restaurant{" "}
